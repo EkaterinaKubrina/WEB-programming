6a76b9bc5fbdb5dddfc7cdc8b3155d32fd1bdc7d
Kubrina Ekaterina <ekaterinakubrina99@gmail.com>
2021-09-26 20:39:08 +0600

    lab2-3

---
+++ lab2-3/rules.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="/css/style.css">
+<title>Правила</title>
+</head>
+<body>
+    <?php include('templates/header.php'); ?>
+
+   <div class="rules">
+   <ul  type="square">
+     <li><a href="/rules1.php">Правила игры</a></li>
+     <li><a href="/rules2.php">Правила записи ходов</a></li>
+   </ul>
+   </div>
+
+   <?php include('templates/footer.php'); ?>
+
+
+</body>
+</html>

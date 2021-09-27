13aa47b6c7d516910c576fe1a7d20018317a1859
Kubrina Ekaterina <ekaterinakubrina99@gmail.com>
2021-09-27 12:53:09 +0600

    correct lab2(9)

--- lab2-3/templates/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <p class="footer-text"> © Екатерина Кубрина </p> 
-    <p class="footer-text"> katty9999@mail.ru </p>                   
-    <p class="footer-text"> г.Омск </p>
-</footer>
--- lab2-3/templates/header.php
+++
@@ -1,9 +0,0 @@
-<header class="site-header">
-    <a href="/" title="" class="logo">
-        <img src="images/chess.png" alt="шахматы" />
-    </a>
-    <nav class="header-categories">
-        <a class="header-category" href="/rules.php">правила</a>
-        <a class="header-category" href="/play.php">играть</a>
-    </nav>
-</header>
